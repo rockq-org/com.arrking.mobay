0841e2ad01b80d6398444331f0048fa1c8bbaed4
icefox0801 <icefox0801@hotmail.com>
2014-11-09 15:53:50 +0800

    #10 fix some jslint error

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -1,3 +1,4 @@
+'use strict';
 /*
  * Licensed Materials - Property of Hai Liang Wang
  * All Rights Reserved.
@@ -5,56 +6,56 @@
 angular.module('mobay.controllers', [])
 
 .controller('LoginCtrl', function($scope, $state, $http, $log, store, cfg, webq) {
-	// check out the sid value and decide which page should be
+    // check out the sid value and decide which page should be
     // navigator.splashscreen.hide();
   //   try{
-		// $state.go('tab.dash');
-  //   	var sid = store.getUserSID();
+        // $state.go('tab.dash');
+  //    var sid = store.getUserSID();
   //   }catch(e){
-  //   	$log.error(e);
+  //    $log.error(e);
   //   }
-	// Form data for the login modal
-	$scope.loginData = {};
-	$scope.doLogin = function(){
-		webq.loginLocalPassport($scope.loginData.username, 
-			$scope.loginData.password).then(function(token){
-			store.setAccessToken(token);
-			$state.go('tab.dash');
-		}, function(err){
-			// TODO show an error message
-			/*
-			 * Possible Cause for login error 
-			 * (1) wrong username and password
-			 * (2) no network
-			 */
-			$log.error(err);
-			$scope.loginData = {};
-		})
-	};
+    // Form data for the login modal
+    $scope.loginData = {};
+    $scope.doLogin = function(){
+        webq.loginLocalPassport($scope.loginData.username,
+            $scope.loginData.password).then(function(token){
+            store.setAccessToken(token);
+            $state.go('tab.dash');
+        }, function(err){
+            // TODO show an error message
+            /*
+             * Possible Cause for login error 
+             * (1) wrong username and password
+             * (2) no network
+             */
+            $log.error(err);
+            $scope.loginData = {};
+        });
+    };
 })
 
 .controller('DashCtrl', function($scope, $state, $log, webq) {
-	webq.getUserProfile().then(function(profile){
-		$log.debug(profile);
-	}, function(err){
-		// maybe token is revoked or has no network
-		// TODO add a utility for get network status
-		if(true){
-			// has no network
-		}else{
-			// token is revoked, force the user login again
-			$state.go('login.form');
-		}
-		$log.error(err);
-	})
+    webq.getUserProfile().then(function(profile){
+        $log.debug(profile);
+    }, function(err){
+        // maybe token is revoked or has no network
+        // TODO add a utility for get network status
+        if(true){
+            // has no network
+        }else{
+            // token is revoked, force the user login again
+            $state.go('login.form');
+        }
+        $log.error(err);
+    });
 })
 
 .controller('NotificationsCtrl', function($scope, Friends) {
-  $scope.friends = Friends.all();
+    $scope.friends = Friends.all();
 })
 
 .controller('NotificationDetailCtrl', function($scope, $stateParams, Friends) {
-  $scope.friend = Friends.get($stateParams.friendId);
+    $scope.friend = Friends.get($stateParams.friendId);
 })
 
 .controller('ProfileCtrl', function($scope) {
